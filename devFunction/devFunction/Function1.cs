883c9cb2d1462aa6d35e63164c1edb7c0b685951
damian <ocampo.damian@hotmail.com>
2025-01-08 23:21:27 -0700

    second commit

--- devFunction/devFunction/Function1.cs
+++ devFunction/devFunction/Function1.cs
@@ -19,7 +19,7 @@ namespace devFunction
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            string name = req.Query["nombre"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
